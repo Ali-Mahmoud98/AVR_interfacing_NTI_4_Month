8f1a3459fcac0d908954f3967b6b6bf809bd4c94
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-08-14 03:39:08 +0300

    Some changes

--- APP_LAYER/0x08-timer/main.c
+++ APP_LAYER/0x08-timer/main.c
@@ -6,32 +6,39 @@
 #include "GIE_interface.h"
 #include "DIO_Interface.h"
 #include "TIMER_interface.h"
-// #include "ADC_interface.h"
+#include "ADC_interface.h"
 #include "LCD_interface.h"
 
-// u8 hr = 0;
-// u8 min = 0;
-u8 sec = 8;
+u8 sec = 0;
+u16 val = 0;
 
 void watch(void);
-void free1(void);
-void free2(void);
+void sendValueToLCD(void);
+void tog_led(void);
 
 int main(void)
 {
     u8 prev = 5;
+    u16 prev2 = 0;
     LCD_init();
 
+    DIO_voidSetPinDirection(DIO_u8_PORTA, DIO_u8_PIN0, DIO_u8_INPUT);
     DIO_voidSetPinDirection(DIO_u8_PORTC, DIO_u8_PIN0, DIO_u8_OUTPUT);
-    //timer0_void_init();
+    timer0_void_init();
     init_void_timer1(NORMAL, NORMAL_OP, CLOCK_256);
 
+    ADC_init();
+    ADC_autoTrigger_Enable();
+    ADC_setCallBack(sendValueToLCD);
+
     setCallBack_OV_T1(watch);
-    // setCallBack_CMPA_T1(free1);
-    // setCallBack_CMPB_T1(free2);
-    GIE_voidGlobalInterruptEnable();
+    
     timer1_interrupt_ENABLE();
-    // timer1_CMPA_Inerrupt_EN();
+    timer0_Interrupt_OverFlow_Enable();
+    setCallBack_OV_T0(tog_led);
+    ADC_interrupt_ENABLE();
+
+    GIE_voidGlobalInterruptEnable();
 
     while (1)
     {
@@ -41,6 +48,18 @@ int main(void)
             LCD_clearScreen();
             LCD_moveCursor(0,0);
             LCD_sendNumber(sec);
+            LCD_moveCursor(1,0);
+            LCD_sendNumber(val);
+        }
+
+        if(prev2 != val)
+        {
+            prev2 = val;
+            LCD_clearScreen();
+            LCD_moveCursor(0,0);
+            LCD_sendNumber(sec);
+            LCD_moveCursor(1,0);
+            LCD_sendNumber(val);
         }
     }
     return 0;
@@ -48,15 +67,22 @@ int main(void)
 
 void watch(void)
 {
-    DIO_voidSetPinValue(DIO_u8_PORTC, DIO_u8_PIN0, HIGH);
     sec++;
 }
 
-void free1(void)
+void sendValueToLCD(void)
 {
-    sec++;
+   val = ADC_readChannel(0);
 }
-void free2(void)
+
+
+void tog_led(void)
 {
-    sec++;
+    static u16 delay = 0;
+    if (delay == (u16)20)
+    {
+        delay = 0;
+        DIO_voidTogglePinValue(DIO_u8_PORTC, DIO_u8_PIN0);
+    }
+    delay++;
 }
